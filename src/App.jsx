43a97ca410f1a7df6d886cbab3071e21fef2dc57
JoseLuiz095 <joseluizacama@gmail.com>
2024-07-25 19:31:03 -0300

    sua mensagem de commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+       <div className="container">
+        <Outlet/>
+       </div>
+    </div>
+
+  )
+}
+
+export default App
